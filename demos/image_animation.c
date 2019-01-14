b07f181787e3c65454cf8624e39137298c371a24
xianjimli <xianjimli@gmail.com>
2019-01-14 17:21:13 +0800

    update comments

--- demos/image_animation.c
+++ demos/image_animation.c
@@ -1,9 +1,9 @@
 /**
- * File:   image.c
+ * File:   image_animation.c
  * Author: AWTK Develop Team
- * Brief:  image demo
+ * Brief:  image_animation demo
  *
- * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,7 @@
 /**
  * History:
  * ================================================================
- * 2018-08-16 Li XianJing <xianjimli@hotmail.com> created
+ * 2019-01-14 Li XianJing <xianjimli@hotmail.com> created
  *
  */
 
