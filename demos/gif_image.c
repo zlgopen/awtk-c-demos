8da0c95bbbcb597691478ea14f47e7dd97344c45
xianjimli <xianjimli@gmail.com>
2019-01-14 12:05:31 +0800

    add gif demo

---
+++ demos/gif_image.c
@@ -0,0 +1,32 @@
+/**
+ * File:   image.c
+ * Author: AWTK Develop Team
+ * Brief:  image demo
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-08-16 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+void application_init() {
+  widget_t* image = NULL;
+  widget_t* win = window_create(NULL, 0, 0, 0, 0);
+
+  image = gif_image_create(win, 10, 10, 200, 200);
+  image_set_image(image, "bee");
+}
+
+#include "demo_main.c"
