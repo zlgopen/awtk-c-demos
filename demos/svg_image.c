eac9db7122c9f5091f3432debbc43beb56591f3c
xianjimli <xianjimli@gmail.com>
2019-01-15 14:55:24 +0800

    add svg_image.c

---
+++ demos/svg_image.c
@@ -0,0 +1,32 @@
+/**
+ * File:   svg_image.c
+ * Author: AWTK Develop Team
+ * Brief:  svg image demo
+ *
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-01-14 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+void application_init() {
+  widget_t* image = NULL;
+  widget_t* win = window_create(NULL, 0, 0, 0, 0);
+
+  image = svg_image_create(win, 10, 10, 200, 200);
+  image_set_image(image, "girl");
+}
+
+#include "demo_main.c"
