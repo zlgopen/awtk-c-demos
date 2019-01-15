025c922808d841e98e32c91d7e02d697ef1ece82
xianjimli <xianjimli@gmail.com>
2019-01-15 15:31:00 +0800

    add text_selector.c

---
+++ demos/text_selector.c
@@ -0,0 +1,40 @@
+/**
+ * File:   text_selector.c
+ * Author: AWTK Develop Team
+ * Brief:  text_selector demo
+ *
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-01-15 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+static ret_t on_changed(void* ctx, event_t* e) {
+  log_debug("on_changed\n");
+  return RET_OK;
+}
+
+void application_init() {
+  widget_t* win = window_create(NULL, 0, 0, 0, 0);
+
+  widget_t* ts = text_selector_create(win, 10, 10, 80, 150);
+  text_selector_set_options(ts, "1:red;2:green;3:blue;4:orange;5:gold");
+  text_selector_set_value(ts, 1);
+  widget_use_style(ts, "dark");
+
+  widget_on(ts, EVT_VALUE_CHANGED, on_changed, NULL);
+}
+
+#include "demo_main.c"
