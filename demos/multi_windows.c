0bb28c05934703ed5cf8a30ff67c8d971efbe2de
xianjimli <xianjimli@gmail.com>
2019-03-12 14:53:12 +0800

    update multi window

--- demos/multi_windows.c
+++ demos/multi_windows.c
@@ -26,7 +26,7 @@ static ret_t on_open_window(void* ctx, event_t* evt) {
   widget_t* label = label_create(win, 0, 0, 0, 0);
   
   widget_set_prop_str(win, WIDGET_PROP_ANIM_HINT, "htranslate");
-  widget_set_text(label, L"Press Escape To CLose");
+  widget_set_text(label, L"Press F2/F3 To CLose");
   widget_set_self_layout_params(label, "center", "middle", "100%", "30");
 
   widget_layout(win);
@@ -34,16 +34,19 @@ static ret_t on_open_window(void* ctx, event_t* evt) {
   return RET_OK;
 }
 
-static ret_t wm_on_key_down(void* ctx, event_t* e) {
+static ret_t on_key_back_or_back_to_home(void* ctx, event_t* e) {
   key_event_t* evt = (key_event_t*)e;
-  if (evt->key == TK_KEY_ESCAPE) {
-    window_manager_request_close_top_window(WIDGET(ctx));
+  if (evt->key == TK_KEY_F2) {
+    window_manager_back(WIDGET(ctx));
+  } else if (evt->key == TK_KEY_F3) {
+    window_manager_back_to_home(WIDGET(ctx));
   }
 
   return RET_OK;
 }
 
 void application_init() {
+  widget_t* wm = window_manager();
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
   widget_t* open_window = button_create(win, 0, 0, 0, 0);
 
@@ -55,7 +58,7 @@ void application_init() {
 
   widget_layout(win);
 
-  widget_on(window_manager(), EVT_KEY_DOWN, wm_on_key_down, window_manager());
+  widget_on(wm, EVT_KEY_DOWN, on_key_back_or_back_to_home, wm);
 }
 
 #include "demo_main.c"
