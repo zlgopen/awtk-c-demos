0b0fea02b6e46da493017411c601e8853d116760
lixianjing <xianjimli@hotmail.com>
2019-07-02 10:58:01 +0800

    fix spin_box type convert

--- demos/spin_box.c
+++ demos/spin_box.c
@@ -37,7 +37,7 @@ static ret_t on_changing(void* ctx, event_t* evt) {
   return RET_OK;
 }
 
-widget_t* create_spin_box(widget_t* win, int type, const char* name, const char* text, int x, int y,
+widget_t* create_spin_box(widget_t* win, input_type_t type, const char* name, const char* text, int x, int y,
                           int w, int h) {
   widget_t* spin_box = spin_box_create(win, x, y, w, h);
 
