27739f292dae262ac63d760def03d64acff4dd3d
lixianjing <xianjimli@hotmail.com>
2019-07-04 14:00:11 +0800

    update mledit.c

--- demos/mledit.c
+++ demos/mledit.c
@@ -1,5 +1,5 @@
 ﻿/**
- * File:   edit.c
+ * File:   mledit.c
  * Author: AWTK Develop Team
  * Brief:  edit demo
  *
@@ -21,21 +21,10 @@
 
 #include "awtk.h"
 
-widget_t* create_edit(widget_t* win, int type, const char* name, const wchar_t* text, int x, int y,
-                      int w, int h) {
-  widget_t* edit = edit_create(win, x, y, w, h);
-
-
-  widget_set_name(edit, name);
-  widget_set_text(edit, text);
-
-  return edit;
-}
-
 static ret_t on_click(void* ctx, event_t* e) {
-  widget_t* edit1 = WIDGET(ctx);
+  widget_t* mledit = WIDGET(ctx);
   
-  widget_set_text(edit1, L"中文　    a b c 123\nnext line");
+  widget_set_text(mledit, L"中文　    a b c 123\nnext line");
 
   return RET_OK;
 }
@@ -43,14 +32,14 @@ static ret_t on_click(void* ctx, event_t* e) {
 void application_init() {
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
 
-  widget_t* edit1 = mledit_create(win, 0, 0, win->w, win->h/2);
+  widget_t* mledit = mledit_create(win, 0, 0, win->w, win->h/2);
   widget_t* button = button_create(win, 0, 0, 0, 0);
 
-  widget_set_text(edit1, L"multi line edit");
+  widget_set_text(mledit, L"multi line edit");
 
   widget_set_text(button, L"SetText");
   widget_set_self_layout_params(button, "c", "b:60", "40%", "30");
-  widget_on(button, EVT_CLICK, on_click, edit1);
+  widget_on(button, EVT_CLICK, on_click, mledit);
 
   widget_layout(win);
 }
