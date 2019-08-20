de5bebb4474b8dac42add3005fce25c43da4ceee
lixianjing <xianjimli@hotmail.com>
2019-08-20 12:46:38 +0800

    update demos

--- demos/combo_box.c
+++ demos/combo_box.c
@@ -21,13 +21,21 @@
 
 #include "awtk.h"
 
+static widget_t* custom_open_popup(widget_t* combo_box) {
+  return window_open("color");
+}
+
 void application_init() {
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
-  widget_t* combo_box = combo_box_create(win, 0, 0, 0, 0);
+  widget_t* combo_box1 = combo_box_create(win, 0, 0, 0, 0);
+  widget_t* combo_box2 = combo_box_create(win, 0, 0, 0, 0);
 
-  combo_box_set_options(combo_box, "left;center;right;");
-  combo_box_set_selected_index(combo_box, 1);
-  widget_set_self_layout_params(combo_box, "center", "middle", "50%", "30");
+  combo_box_set_options(combo_box1, "left;center;right;");
+  combo_box_set_selected_index(combo_box1, 1);
+  widget_set_self_layout_params(combo_box1, "center", "middle:-30", "50%", "30");
+
+  combo_box_set_custom_open_popup(combo_box2, custom_open_popup);
+  widget_set_self_layout_params(combo_box2, "center", "middle:30", "50%", "30");
 
   widget_layout(win);
 }
