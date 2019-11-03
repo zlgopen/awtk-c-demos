9dee5b002bb4f18a91b5df85912d69df2a3930d6
lixianjing <xianjimli@hotmail.com>
2019-11-03 17:55:17 +0800

    improve demo

--- demos/image_value.c
+++ demos/image_value.c
@@ -21,13 +21,26 @@
 
 #include "awtk.h"
 
+static ret_t on_timer_update(const timer_info_t* timer) {
+  widget_t* win = WIDGET(timer->ctx);
+  widget_t* image_value = widget_lookup(win, "image_value01", TRUE);
+
+  image_value_set_value(image_value, IMAGE_VALUE(image_value)->value + 1);
+
+  return RET_REPEAT;
+}
+
 void application_init() {
   widget_t* image_value = NULL;
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
 
   image_value = image_value_create(win, 10, 10, 200, 200);
+
+  widget_set_name(image_value, "image_value01");
   image_value_set_image(image_value, "num_");
   image_value_set_value(image_value, 100);
+
+  timer_add(on_timer_update, win, 1000);
 }
 
 #include "demo_main.c"
