a930c6e5b21cece6c6b2e7a61b6ca1bbcfc53baa
xianjimli <xianjimli@gmail.com>
2020-03-19 08:27:40 +0800

    update assets

--- sync_assets.sh
+++ sync_assets.sh
@@ -2,6 +2,6 @@
 
 cp -rf ../awtk/demos/assets .
 cp -fv ../awtk/demos/assets.c demos
-cp -fv ../awtk/demos/assets_dark.c demos
-cp -fv ../awtk/demos/assets_default.c demos
+cp -fv ../awtk/demos/assets_dark.inc demos
+cp -fv ../awtk/demos/assets_default.inc demos
 
