e5a07b9a1ca4762cba82b8bfa13c05ce01b61d13
xianjimli <xianjimli@gmail.com>
2020-06-14 08:30:54 +0800

    improve list_item_clone.c

--- demos/list_item_clone.c
+++ demos/list_item_clone.c
@@ -23,7 +23,7 @@
 
 static ret_t on_click(void* ctx, event_t* e) {
   uint32_t i = 0;
-  uint32_t n = 100 * 10000;
+  uint32_t n = 10000;
   widget_t* view = WIDGET(ctx);
   widget_t* item = widget_get_child(view, 0);
   uint64_t start = time_now_ms();
@@ -39,6 +39,40 @@ static ret_t on_click(void* ctx, event_t* e) {
   return RET_OK;
 }
 
+static ret_t on_scroll_start(void* ctx, event_t* e) {
+  scroll_view_t* view = SCROLL_VIEW(e->target);
+
+  log_debug("scroll start: %d\n", view->yoffset);
+
+  return RET_OK;
+}
+
+static ret_t on_scroll_end(void* ctx, event_t* e) {
+  widget_t* widget = WIDGET(e->target);
+  scroll_view_t* view = SCROLL_VIEW(widget);
+  widget_t* item = widget_get_child(widget, 0);
+
+  log_debug("scroll end: yoffset=%d virtual_h=%d h=%d\n", view->yoffset, view->virtual_h, widget->h);
+
+  if((view->yoffset + widget->h) >= view->virtual_h) {
+    int32_t i = 0;
+
+    for(i = 0; i < 20; i++) {
+      widget_clone(item, widget);
+    }
+  }
+
+  return RET_OK;
+}
+
+static ret_t on_scroll(void* ctx, event_t* e) {
+  scroll_view_t* view = SCROLL_VIEW(e->target);
+
+  log_debug("scroll: %d\n", view->yoffset);
+
+  return RET_OK;
+}
+
 ret_t application_init() {
   widget_t* win = window_open("list_view_m");
   widget_t* view = widget_lookup_by_type(win, WIDGET_TYPE_SCROLL_VIEW, TRUE);
@@ -46,6 +80,9 @@ ret_t application_init() {
 
   widget_set_text_utf8(button, "Clone");
   widget_on(button, EVT_CLICK, on_click, view);
+  widget_on(view, EVT_SCROLL_START, on_scroll_start, view);
+  widget_on(view, EVT_SCROLL_END, on_scroll_end, view);
+  widget_on(view, EVT_SCROLL, on_scroll, view);
 
   return RET_OK;
 }
