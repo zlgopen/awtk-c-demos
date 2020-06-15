c36cf100200d5d5fd062c847c18838a7f06d73c9
lixianjing <xianjimli@hotmail.com>
2020-06-15 18:47:15 +0800

    update demos

--- demos/wait_cursor.c
+++ demos/wait_cursor.c
@@ -28,7 +28,7 @@ static ret_t on_wait_done(const timer_info_t* info) {
   return RET_REMOVE;
 }
 
-static ret_t on_click(void* ctx, event_t* e) {
+static ret_t click_to_wait(void* ctx, event_t* e) {
   widget_t* button = WIDGET(e->target);
   widget_begin_wait_pointer_cursor(button, TRUE);
 
@@ -37,16 +37,27 @@ static ret_t on_click(void* ctx, event_t* e) {
   return RET_OK;
 }
 
+static ret_t click_to_quit(void* ctx, event_t* e) {
+  tk_quit();
+
+  return RET_OK;
+}
 
 ret_t application_init() {
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
-  widget_t* ok = button_create(win, 0, 0, 0, 0);
+  widget_t* button = button_create(win, 0, 0, 0, 0);
 
   locale_info_change(locale_info(), "zh", "CN");
 
-  widget_set_tr_text(ok, "ok");
-  widget_set_self_layout_params(ok, "center", "middle", "50%", "30");
-  widget_on(ok, EVT_CLICK, on_click, NULL);
+  widget_set_tr_text(button, "Button");
+  widget_set_self_layout_params(button, "center", "middle", "50%", "30");
+  widget_on(button, EVT_CLICK, click_to_wait, NULL);
+  
+  button = button_create(win, 0, 0, 0, 0);
+  widget_set_tr_text(button, "Quit");
+  widget_set_self_layout_params(button, "center", "middle:50", "50%", "30");
+  widget_set_prop_str(button, WIDGET_PROP_POINTER_CURSOR, "cursor_hand");
+  widget_on(button, EVT_CLICK, click_to_quit, NULL);
 
   widget_layout(win);
 
