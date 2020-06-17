54a0f1c71511757cde25998d7d1a9ac4b6a1cffb
lixianjing <xianjimli@hotmail.com>
2020-06-17 11:10:07 +0800

    update demos

--- demos/label_chinese.c
+++ demos/label_chinese.c
@@ -29,6 +29,8 @@
  */
 ret_t application_init() {
   char str[256];
+  char car_no_utf8[24];
+  const wchar_t* car_no = L"粤B12345";
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
   widget_t* label = label_create(win, 10, 10, win->w - 20, win->h - 20);
 
@@ -36,8 +38,9 @@ ret_t application_init() {
   const char* type2 = "天然气";
   const char* type3 = "自动";
 
+  tk_utf8_from_utf16(car_no, car_no_utf8, sizeof(car_no_utf8));
   tk_snprintf(str, sizeof(str),
-              "当前状态:车牌%s；\n燃料[%s]；\n额定转速[%drpm]；\n调零[%s]；\n泄露[%s]", "ABCDE",
+              "当前状态:车牌%s；\n燃料[%s]；\n额定转速[%drpm]；\n调零[%s]；\n泄露[%s]", car_no_utf8,
               type1, 1024, type2, type3);
 
   widget_set_text_utf8(label, str);
