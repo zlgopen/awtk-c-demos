7f8201d0e497fc0e48863d484c7cc5226ec48678
lixianjing <xianjimli@hotmail.com>
2021-03-10 16:03:49 +0800

    add unload_font.c

---
+++ demos/unload_font.c
@@ -0,0 +1,66 @@
+﻿/**
+ * File:   button.c
+ * Author: AWTK Develop Team
+ * Brief:  button demo
+ *
+ * Copyright (c) 2018 - 2021  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-08-16 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+static ret_t on_click(void* ctx, event_t* e) {
+  widget_t* widget = WIDGET(e->target);
+
+  widget_set_text(widget, L"OK");
+
+  widget_set_style_str(widget, "normal:font_name", "ap");
+  widget_set_style_str(widget, "over:font_name", "ap");
+  widget_set_style_str(widget, "pressed:font_name", "ap");
+  widget_set_style_str(widget, "disable:font_name", "ap");
+
+  font_manager_unload_font(font_manager(), "default", 0);
+
+  log_debug("on_click\n");
+  return RET_OK;
+}
+
+static ret_t on_long_press(void* ctx, event_t* e) {
+  log_debug("on_long_press\n");
+  return RET_OK;
+}
+
+ret_t application_init() {
+  widget_t* win = window_create(NULL, 0, 0, 0, 0);
+  widget_t* ok = button_create(win, 0, 0, 0, 0);
+
+  locale_info_change(locale_info(), "zh", "CN");
+
+  widget_set_tr_text(ok, "ok");
+  widget_set_self_layout_params(ok, "center", "middle", "50%", "30");
+  widget_on(ok, EVT_CLICK, on_click, NULL);
+  widget_on(ok, EVT_LONG_PRESS, on_long_press, NULL);
+
+  widget_layout(win);
+
+  return RET_OK;
+}
+
+ret_t application_exit() {
+  log_debug("application_exit\n");
+  return RET_OK;
+}
+
+#include "awtk_main.inc"
