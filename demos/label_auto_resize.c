6d6696c484790cd29e648d99ac856262a8659405
lixianjing <xianjimli@hotmail.com>
2021-03-13 07:29:10 +0800

    improve demos

--- demos/label_auto_resize.c
+++ demos/label_auto_resize.c
@@ -23,11 +23,17 @@
 
 ret_t application_init() {
   widget_t* win = window_create(NULL, 0, 0, 0, 0);
-  widget_t* label = label_create(win, 10, 10, 200, 30);
+  widget_t* label1 = label_create(win, 10, 10, 200, 30);
+  widget_t* label2 = label_create(win, 10, 60, 200, 30);
 
-  widget_use_style(label, "big_green");
-  widget_set_text(label, L"Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium!");
-  widget_set_auto_adjust_size(label, TRUE);
+  widget_set_text(label1, L"Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium!");
+  widget_set_auto_adjust_size(label1, TRUE);
+
+  widget_set_text(label2, L"Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium!");
+
+  label_set_line_wrap(label2, TRUE);
+  label_set_word_wrap(label2, TRUE);
+  widget_set_auto_adjust_size(label2, TRUE);
 
   widget_layout(win);
 
