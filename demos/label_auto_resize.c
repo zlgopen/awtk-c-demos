6b92e7091c6dc8383901b066ee7740e0fba87f7a
lixianjing <xianjimli@hotmail.com>
2021-03-12 16:31:51 +0800

    add demos

---
+++ demos/label_auto_resize.c
@@ -0,0 +1,42 @@
+﻿/**
+ * File:   label.c
+ * Author: AWTK Develop Team
+ * Brief:  label demo
+ *
+ * Copyright (c) 2018 - 2021  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-08-15 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+
+ret_t application_init() {
+  widget_t* win = window_create(NULL, 0, 0, 0, 0);
+  widget_t* label = label_create(win, 10, 10, 200, 30);
+
+  widget_use_style(label, "big_green");
+  widget_set_text(label, L"Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium!");
+  widget_set_auto_adjust_size(label, TRUE);
+
+  widget_layout(win);
+
+  return RET_OK;
+}
+
+ret_t application_exit() {
+  log_debug("application_exit\n");
+  return RET_OK;
+}
+
+#include "awtk_main.inc"
